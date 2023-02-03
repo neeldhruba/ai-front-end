a5ec210e94616b2643309609cd5409e11c37ecf3
spectrebond <spectrebond200@gmail.com>
2023-02-03 10:45:18 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import log from './log.jpg';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  log,
+  preview,
+};
---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+import FileSaver from 'file-saver';
+import {surpriseMePrompts} from '../constants'
+
+export function getRandomPrompt(prompt){
+    const randomIndex = Math.floor(Math.random() * 
+    surpriseMePrompts.length);
+    const randomPromt = surpriseMePrompts[randomIndex];
+
+    if(randomPromt === prompt) return getRandomPrompt(prompt)
+    
+
+    return randomPromt;
+}
+
+
+export async function downloadIamge(_id, photo){
+    FileSaver.saveAs(photo,`download-${_id}.jpg`);
+    
+}
